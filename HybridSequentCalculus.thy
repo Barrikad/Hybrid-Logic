ad19886f0ebb1382445ed7e5d98bf2163feac8d8
Barrikad <s184459@student.dtu.dk>
2021-04-11 18:43:34 +0200

    now we just have non-termination

--- HybridSequentCalculus.thy
+++ HybridSequentCalculus.thy
@@ -1,19 +1,23 @@
 theory HybridSequentCalculus imports Main
 begin
 
-fun f :: \<open>nat \<Rightarrow> nat\<close> where
-  \<open>f 0 = (2)\<close> |
-  \<open>f (Suc 0) = (4)\<close> |
-  \<open>f _ = 2\<close>
-
-
 primrec member where
   \<open>member _ [] = False\<close> |
   \<open>member m (n # A) = (m = n \<or> member m A)\<close>
 
+primrec common where
+  \<open>common _ [] = False\<close> |
+  \<open>common X (y # Y) = (member y X \<or> common X Y)\<close>
+
 lemma member_iff [iff]: \<open>member m A \<longleftrightarrow> m \<in> set A\<close>
   by (induct A) simp_all
 
+lemma common_iff [iff]: \<open>common A B \<longleftrightarrow> set A \<inter> set B \<noteq> {}\<close>
+  by (induct B) simp_all
+
+datatype 'a option
+  = None
+  | Some 'a
 
 datatype 'a hybr_form 
   =  Pro 'a (\<open>PRO _\<close> [999] 999)
@@ -22,6 +26,15 @@ datatype 'a hybr_form
   | Con \<open>'a hybr_form\<close> \<open>'a hybr_form\<close> (infixl "AND" 300)
   | Sat nat \<open>'a hybr_form\<close> (\<open>AT _ _\<close> [899] 899)
   | Pos \<open>'a hybr_form\<close> (\<open>\<diamond> _\<close> [800] 800)
+
+(*size definition for proving termination of atomize*)
+primrec size' :: \<open>'a hybr_form \<Rightarrow> nat\<close> where
+  \<open>size' (Pro a) = Suc 0\<close> |
+  \<open>size' (Nom n) = Suc 0\<close> |
+  \<open>size' (Neg p) = Suc (size' p)\<close> |
+  \<open>size' (Con p1 p2) = Suc (size' p1 + size' p2)\<close> |
+  \<open>size' (Sat n p) = Suc (Suc (size' p))\<close> |
+  \<open>size' (Pos p) = Suc (Suc (size' p))\<close>
 
 primrec semantics :: \<open>'c set \<Rightarrow> ('c \<Rightarrow> 'c \<Rightarrow> bool) \<Rightarrow> ('c \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow>
                   (nat \<Rightarrow> 'c) \<Rightarrow> 'c \<Rightarrow> 'a hybr_form \<Rightarrow> bool\<close> where
@@ -57,160 +70,134 @@ primrec nominalsForms where
   \<open>nominalsForms [] = {}\<close> |
   \<open>nominalsForms (x # xs) = nominalsForm x \<union> (nominalsForms xs)\<close>
 
-fun nominalsNN :: \<open>(nat list \<times> nat) list \<Rightarrow> nat set\<close> where
+fun nominalsNN where
   \<open>nominalsNN [] = {}\<close> |
-  \<open>nominalsNN ((XS,n) # NN) = {n} \<union> (set XS) \<union> (nominalsNN NN)\<close>
+  \<open>nominalsNN ((n1,n2) # NN) = {n1,n2} \<union> (nominalsNN NN)\<close>
 
-fun nominalsNA :: \<open>(nat list \<times> 'a) list \<Rightarrow> nat set\<close> where
+fun nominalsNA where
   \<open>nominalsNA [] = {}\<close> |
-  \<open>nominalsNA ((XS,a) # NA) = (set XS) \<union> nominalsNA NA\<close>
+  \<open>nominalsNA ((n,a) # NA) = {n} \<union> nominalsNA NA\<close>
 
-fun nominalsNP :: \<open>(nat list \<times> ('a hybr_form)) list \<Rightarrow> nat set\<close> where
+fun nominalsNP where
   \<open>nominalsNP [] = {}\<close> |
-  \<open>nominalsNP ((XS,p) # NP) = (set XS) \<union> (nominalsForm p) \<union> (nominalsNP NP)\<close>
+  \<open>nominalsNP ((n,p) # NP) = {n} \<union> (nominalsForm p) \<union> (nominalsNP NP)\<close>
 
-(*get all used nominals on one side of a sequent*)
-abbreviation used where 
-  \<open>used X Y Z Q \<equiv> nominalsNA X \<union> (nominalsNN Y) \<union> (nominalsNN Z) \<union> (nominalsNP Q)\<close>
+fun nominalsNSNP where
+  \<open>nominalsNSNP [] = {}\<close> |
+  \<open>nominalsNSNP ((ns,n,p) # NSNP) = set ns \<union> {n} \<union> nominalsForm p \<union> nominalsNSNP NSNP\<close>
 
 (*merge functions replaces all occurences of a nominal with another
 use when two nominals are found to denote the same world*)
-
-primrec mergeN where
-  \<open>mergeN [] na nb = []\<close> |
-  \<open>mergeN (n # XS) na nb = (if na = n then nb else n) # mergeN XS na nb\<close>
+fun mergeNS where 
+  \<open>mergeNS [] na nb = []\<close> |
+  \<open>mergeNS (n # NS) na nb = (if n = na then nb else n) # (mergeNS NS na nb)\<close>
 
 fun mergeNN where
   \<open>mergeNN [] na nb = []\<close> |
-  \<open>mergeNN ((XS,n) # NN) na nb = (mergeN XS na nb, if n = na then nb else n) # mergeNN NN na nb\<close>
+  \<open>mergeNN ((n1,n2) # NN) na nb = (
+    let n3 = (if n1 = na then nb else n1) in 
+    let n4 = (if n2 = na then nb else n2) in 
+    (n3,n4) # (mergeNN NN na nb))\<close>
 
 fun mergeNA where
   \<open>mergeNA [] na nb = []\<close> |
-  \<open>mergeNA ((XS,a) # NA) na nb = (mergeN XS na nb, a) # (mergeNA NA na nb)\<close>
+  \<open>mergeNA ((n1,a) # NA) na nb = (if n1 = na then nb else n1, a) # (mergeNA NA na nb)\<close>
 
-fun nameCur where
-  \<open>nameCur [] n = []\<close> |
-  \<open>nameCur (([],a) # X) n = ([n],a) # nameCur X n\<close> |
-  \<open>nameCur ((x # XS,a) # X) n = (x # XS,a) # nameCur X n\<close>
+fun mergeP where
+  \<open>mergeP (Pro a) na nb = Pro a\<close> |
+  \<open>mergeP (Nom n) na nb = Nom (if n = na then nb else n)\<close> |
+  \<open>mergeP (Neg p) na nb = Neg (mergeP p na nb)\<close> |
+  \<open>mergeP (Con p1 p2) na nb = Con (mergeP p1 na nb) (mergeP p2 na nb)\<close> |
+  \<open>mergeP (Sat n p) na nb = Sat (if n = na then nb else n) (mergeP p na nb)\<close> |
+  \<open>mergeP (Pos p) na nb = Pos (mergeP p na nb)\<close>
 
-(*removes all statements about world refrenced by nw
-if other worlds depend on being reached from nw, remove them too
-use when it is shown that a world we assumed existed doesn't exist*)
-function purge'
-where
-  \<open>purge' X A B Z ((XS,n) # C) X' A' B' Z' C' nw = 
-    (let ((X'',A'',B'',Z'',C''),C''') =
-      (if member nw XS then 
-      (purge' X A B Z C [] [] [] [] [] n,C') 
-       else ((X,A,B,Z,C),(XS,n) # C')) in
-    purge' X'' A'' B'' Z'' C'' X' A' B' Z' C''' nw)\<close> |
-  \<open>purge' X A B ((XS,n) # Z) [] X' A' B' Z' C' nw = 
-    (if member nw XS then 
-    purge' X A B Z [] X' A' B' Z' C' nw else
-    purge' X A B Z [] X' A' B' ((XS,n) # Z') C' nw)\<close>|
-  \<open>purge' X A ((XS,n) # B) [] [] X' A' B' Z' C' nw = 
-    (if member nw XS then 
-    purge' X A B [] [] X' A' B' Z' C' nw else
-    purge' X A B [] [] X' A' ((XS,n) # B') Z' C' nw)\<close>|
-  \<open>purge' X ((XS,a) # A) [] [] [] X' A' B' Z' C' nw = 
-    (if member nw XS then 
-    purge' X A [] [] [] X' A' B' Z' C' nw else
-    purge' X A [] [] [] X' ((XS,a) # A') B' Z' C' nw)\<close>|
-  \<open>purge' ((XS,a) # X) [] [] [] [] X' A' B' Z' C' nw = 
-    (if member nw XS then 
-    purge' X [] [] [] [] X' A' B' Z' C' nw else
-    purge' X [] [] [] [] ((XS,a) # X') A' B' Z' C' nw)\<close>|
-  \<open>purge' [] [] [] [] [] X' A' B' Z' C' nw = (X',A',B',Z',C')\<close> 
-by pat_completeness auto
-termination 
-  apply (relation \<open>measure (\<lambda>(X,A,B,Z,C,_,_,_,_,_,_). size X + size A + size B + size Z + size C)\<close>)
-  sorry
-(*termination should be straight forward. Need to find out what fails*)
+fun mergeNP where
+  \<open>mergeNP [] na nb = []\<close> |
+  \<open>mergeNP ((n,p) # NP) na nb = (if n = na then nb else n, mergeP p na nb) # (mergeNP NP na nb)\<close>
 
-abbreviation purge where \<open>purge X A B Z C \<equiv> purge' X A B Z C [] [] [] [] []\<close>
+fun mergeNSNP where
+  \<open>mergeNSNP [] na nb = []\<close> |
+  \<open>mergeNSNP ((ns,n,p) # R) na nb = 
+    (mergeNS ns na nb, if n = na then nb else n, mergeP p na nb) # (mergeNSNP R na nb)\<close>
 
-fun match where
-  \<open>match [] [] = True\<close> |
-  \<open>match [] _ = False\<close> |
-  \<open>match _ [] = False\<close> |
-  \<open>match (n1 # XS) (n2 # YS) = (n1 = n2)\<close>
+fun find_non_common where
+  \<open>find_non_common _ [] = None\<close> |
+  \<open>find_non_common ms (n # ns) = (if \<not>member n ns then Some n else find_non_common ms ns)\<close>
 
-fun findAtom where
-  \<open>findAtom [] n a = False\<close> |
-  \<open>findAtom (([], b) # X) n a = findAtom X n a\<close> |
-  \<open>findAtom ((n1 # XS, b) # X) n2 a = ((b = a \<and> n1 = n2) \<or> findAtom X n2 a)\<close>
-  
-fun pvr :: "(nat list \<times> 'a) list \<Rightarrow> (nat list \<times> 'a) list \<Rightarrow> (nat list \<times> nat) list \<Rightarrow> bool" where
-  (*@a b on RSH holds if a=b*)
-  \<open>pvr X A (([],n) # B) = pvr X A B\<close> |
-  \<open>pvr X A ((n1 # XS,n2) # B) = (n1 = n2 \<or> pvr X A B)\<close> |
-  (*A proposition on a world holds if it's on both LHS and RHS*)
-  \<open>pvr X (([],a) # A) [] = (member ([],a) X \<or> pvr X A [])\<close> |
-  \<open>pvr X ((n # XS,a) # A) [] =(findAtom X n a \<or> pvr X A [])\<close> |
-  (*If we reach this point, we couldn't show that the sequent is valid*)
-  \<open>pvr _ [] [] = False\<close>
+fun saturate' where
+  \<open>saturate' R' [] ns = None\<close> |
+  \<open>saturate' R' ((ms,n,p) # R) ns = (
+    case find_non_common ms ns of
+      None   \<Rightarrow> saturate' ((ms,n,p) # R') R ns
+    | Some m \<Rightarrow> Some (n,m,p,R' @ R,R' @ [(m # ms,n,p)] @ R))\<close>
 
-function reach and witness where
-  (*merge equal nominals*)
-  \<open>reach X A B Z C (([],n) # Y) = reach (nameCur X n) (nameCur A n) (nameCur B n) 
-                                        (nameCur Z n) (nameCur C n) (nameCur Y n)\<close> |
-  \<open>reach X A B Z C ((n1 # XS,n2) # Y) = reach (mergeNA X n1 n2) (mergeNA A n1 n2) 
-                                              (mergeNN B n1 n2) (mergeNN Z n1 n2) 
-                                              (mergeNN C n1 n2) (mergeNN Y n1 n2)\<close> |
+definition saturate where \<open>saturate R ns \<equiv> saturate' [] R ns\<close>
 
-  (*find witnesses for possibility on RHS. 
-   If no witness can be found, purge the node and try with next*)
-  \<open>reach X A B Z ((XS,nw) # C) [] = (witness X A B C Z (XS,nw) Z 
-                                    @(let (X',A',B',Z',C') = purge X A B Z C nw in 
-                                      reach X' A' B' Z' C' []))\<close> |
-  \<open>reach X A B _ [] [] = [(X,A,B)]\<close> |
-  (*can't find witness if nothing is reachable*)
-  \<open>witness X A B C Z2 (XS,nw) [] = []\<close> |
-  (*if n2 is reachable from n, then check if p holds at n2*)
-  \<open>witness X A B C Z2 (XS,nw) ((YS,n2) # Z) = ((if match XS YS 
-                                                then reach X A B Z2 C [([nw],n2)]
-                                                else [])
-                                                @ witness X A B C Z2 (XS,nw) Z)\<close> 
-  by pat_completeness auto
-termination sorry
+fun reflect where
+  \<open>reflect [] = False\<close> |
+  \<open>reflect ((n1,n2) # B) = (n1 = n2 \<or> reflect B)\<close>
 
 function atomize where
   (*match RHS*)
-  \<open>atomize X A Y B Z C Q ((XS,Pro a) # P)     = atomize X ((XS,a) # A) Y B Z C Q P\<close> |
-  \<open>atomize X A Y B Z C Q ((XS,Nom n) # P)   = atomize X A Y ((XS,n) # B) Z C Q P\<close> |
-  \<open>atomize X A Y B Z C Q ((XS,Neg p) # P)     = atomize X A Y B Z C ((XS,p) # Q) P\<close> |
-  \<open>atomize X A Y B Z C Q ((XS,Con p1 p2) # P) =((atomize X A Y B Z C Q ((XS,p1) # P)) 
-                                                  @ (atomize X A Y B Z C Q ((XS,p2) # P)))\<close> |
-  \<open>atomize X A Y B Z C Q ((XS,Sat n p) # P) = atomize X A Y B Z C Q ((n # XS,p) # P)\<close> |
-  \<open>atomize X A Y B Z C Q ((XS,Pos p) # P)     =
-          (let nw = fresh (used X Y Z ((XS,Pos p) # P) \<union> (used A B C Q)) in
-          (atomize X A Y B Z ((XS,nw) # C) Q ((nw # XS,p) # P)))\<close> |
+  \<open>atomize LA RA RN LP RP R Q ((n,Pro a) # P) = 
+    atomize LA ((n,a) # RA) RN LP RP R Q P\<close> |
+
+  \<open>atomize LA RA RN LP RP R Q ((n1,Nom n2) # P) = 
+    atomize LA RA ((n1,n2) # RN) LP RP R Q P\<close> |
+
+  \<open>atomize LA RA RN LP RP R Q ((n,Neg p) # P) = 
+    atomize LA RA RN LP RP R ((n,p) # Q) P\<close> |
+
+  \<open>atomize LA RA RN LP RP R Q ((n,Con p1 p2) # P) =
+    ((atomize LA RA RN LP RP R Q ((n,p1) # P)) \<and> (atomize LA RA RN LP RP R Q ((n,p2) # P)))\<close> |
+
+  \<open>atomize LA RA RN LP RP R Q ((n1,Sat n2 p) # P) = 
+    atomize LA RA RN LP RP R Q ((n2,p) # P)\<close> |
+
+  \<open>atomize LA RA RN LP RP R Q ((n,Pos p) # P) = 
+    atomize LA RA RN LP RP (([],n,p) # R) Q P\<close>|
+
   (*match LHS*)
-  \<open>atomize X A Y B Z C ((XS,Pro a) # Q) []    = atomize ((XS,a) # X) A Y B Z C Q []\<close> |
-  \<open>atomize X A Y B Z C ((XS,Nom n) # Q) []  = atomize X A ((XS,n) # Y) B Z C Q []\<close> |
-  \<open>atomize X A Y B Z C ((XS,Neg p) # Q) []    = atomize X A Y B Z C Q [(XS,p)]\<close> |
-  \<open>atomize X A Y B Z C ((XS,Con p1 p2) # Q) []
-        = atomize X A Y B Z C ((XS,p1) # (XS,p2) # Q) []\<close> |
-  \<open>atomize X A Y B Z C ((XS,Sat n p) # Q) []= atomize X A Y B Z C ((n # XS,p) # Q) []\<close> |
-  \<open>atomize X A Y B Z C ((XS,Pos p) # Q) []    =
-          (let nw = fresh (used X Y Z Q \<union> (used A B C Q)) in
-          (atomize X A Y B ((XS,nw) # Z) C ((nw # XS,p) # Q) []))\<close> |
-  \<open>atomize X A Y B Z C [] []                  = [(X,A,Y,B,Z,(rev C))]\<close>
-  by pat_completeness auto
-termination sorry
-  (*by (relation \<open>measure (\<lambda>(_,_,_,_,_,_,Q,P). \<Sum>p \<leftarrow> Q @ P. (size p))\<close>) 
-               (auto simp add: Let_def)*)
+  \<open>atomize LA RA RN LP RP R ((n,Pro a) # Q) [] = 
+    atomize ((n,a) # LA) RA RN LP RP R Q []\<close> |
 
-fun eval2 where
-  \<open>eval2 [] = False\<close> |
-  \<open>eval2 ((X,A,B) # XS) = (pvr X A B \<or> eval2 XS)\<close>
+  \<open>atomize LA RA RN LP RP R ((n1,Nom n2) # Q) [] = 
+    atomize (mergeNA LA n1 n2) (mergeNA RA n1 n2) (mergeNN RN n1 n2) 
+      (mergeNN LP n1 n2) (mergeNN RP n1 n2) (mergeNSNP R n1 n2) (mergeNP Q n1 n2) []\<close> |
 
-fun eval1 where
-  \<open>eval1 [] = True\<close> |
-  \<open>eval1 ((X,A,Y,B,Z,C) # XS) = (eval2 (reach X A B Z C Y) \<and> eval1 XS)\<close>
+  \<open>atomize LA RA RN LP RP R ((n,Neg p) # Q) [] = 
+    atomize LA RA RN LP RP R Q [(n,p)]\<close> |
+
+  \<open>atomize LA RA RN LP RP R ((n,Con p1 p2) # Q) []= 
+    atomize LA RA RN LP RP R ((n,p1) # (n,p2) # Q) []\<close> |
+
+  \<open>atomize LA RA RN LP RP R ((n1,Sat n2 p) # Q) [] = 
+    atomize LA RA RN LP RP R ((n2,p) # Q) []\<close> |
+
+  \<open>atomize LA RA RN LP RP R ((n,Pos p) # Q) [] = (
+    let nw = fresh (
+      nominalsNA LA \<union> nominalsNA RA \<union> nominalsNN RN \<union> nominalsNN LP \<union> 
+      nominalsNN RP \<union> nominalsNSNP R \<union> nominalsNP ((n,Pos p) # Q)) 
+    in (atomize LA RA RN ((n,nw) # LP) RP R ((nw,p) # Q) []))\<close> |
+
+  \<open>atomize LA RA RN LP RP R [] [] = (
+      case 
+        saturate R (sorted_list_of_set (
+          nominalsNA LA \<union> nominalsNA RA \<union> nominalsNN RN \<union> 
+          nominalsNN LP \<union> nominalsNN RP \<union> nominalsNSNP R)) 
+      of
+        None \<Rightarrow> (common LA RA \<or> common LP RP \<or> reflect RN)
+      | Some (n,m,p,R',R'') \<Rightarrow> 
+          (atomize LA RA RN LP ((n,m) # RP) R' [] [] \<and> atomize LA RA RN LP RP R' [] [(m,p)]) 
+          \<or> atomize LA RA RN LP RP R'' [] [])\<close> 
+  by pat_completeness simp_all
+termination 
+  apply (relation \<open>measure (\<lambda>(_,_,_,_,_,_,Q,P). \<Sum>(_,p) \<leftarrow> Q @ P. size' p)\<close>) 
+  sorry
 
 (*tautology definition*)
-definition prover where \<open>prover p \<equiv> eval1 (atomize [] [] [] [] [] [] [] [([],p)])\<close>
+definition prover where 
+  \<open>prover p \<equiv> atomize [] [] [] [] [] [] [] [(fresh (nominalsForm p),p)]\<close>
 
 (*abbreviations*)
 abbreviation Imp (infixr "THEN"  180)
@@ -273,10 +260,19 @@ proposition \<open>prover ((AT (1) (\<diamond> NOM 2)) AND
 proposition \<open>prover ((AT (1) (\<diamond>\<diamond>\<diamond>\<diamond> Pro A)) THEN (AT (1) (\<diamond>\<diamond>\<diamond>\<diamond> Pro A)))\<close>
   by eval
 
-proposition \<open>prover ((AT (1) ((NOM 1) AND 
-                     (AT (2) ((NOM 2) AND 
+proposition \<open>prover ((AT (1) ( 
+                     (AT (2) (
                      ((\<box>((Pro A) THEN ((NOT ((Pro A) THEN ((Pro B) THEN (Pro A)))) OR (Pro B)))) 
                       THEN ((\<box>(Pro A)) THEN \<box> (Pro B))))))))\<close>
+  by eval
+
+proposition \<open>prover ((\<diamond> NOT PRO A) OR (\<box> PRO A))\<close>
+  by eval
+
+proposition \<open>prover ((\<diamond> PRO A) OR (NOT (\<diamond> PRO A)))\<close>
+  by eval
+
+proposition \<open>prover ((NOT (\<diamond> PRO A)) OR (\<diamond> PRO A))\<close>
   by eval
 
 proposition \<open>prover ((AT (1) (NOT (\<diamond> NOM (1)))) THEN 
@@ -285,6 +281,21 @@ proposition \<open>prover ((AT (1) (NOT (\<diamond> NOM (1)))) THEN
   by eval
 
 proposition \<open>prover ((\<diamond>PRO A) THEN (\<diamond>PRO A))\<close>
+  by eval
+
+proposition \<open>prover ((PRO A) AND (AT 1 (\<diamond> NOM 2)) AND (AT 2 (\<diamond> NOM 1)) THEN (AT 1 (\<diamond>\<diamond> NOM 1)))\<close>
+  by eval
+
+proposition \<open>prover ((\<diamond> PRO A) THEN (\<diamond> PRO B) OR (\<diamond> NOT PRO B))\<close>
+  by eval
+
+proposition \<open>prover ((\<diamond> PRO A) THEN (\<diamond> NOT PRO B) OR (\<diamond> PRO B))\<close>
+  by eval
+
+proposition \<open>prover ((\<diamond> PRO A) THEN (\<diamond> (AT 2 NOT (\<diamond> NOM 1))) OR (AT 2 \<diamond> NOM 1))\<close>
+  by eval
+
+proposition \<open>prover ((\<diamond> PRO A) THEN (AT 2 \<diamond> NOM 1 ) OR (\<diamond> (AT 2 NOT (\<diamond> NOM 1))))\<close>
   by eval
 
 (*invalid tests*)
@@ -327,6 +338,8 @@ proposition \<open>\<not>prover ((AT (2) (Pro A)) THEN (AT (1) (\<diamond> (AT (
 proposition \<open>\<not>prover ((Pro A) THEN (Sat (1) (Pro A)))\<close>
   by eval
 
+proposition \<open>\<not>prover (AT 1 (\<diamond> ((NOT (AT 1 (\<diamond> NOM 2))) OR (PRO A OR NOT PRO A))))\<close>
+  by eval
 
 (*export*)
 definition \<open>main \<equiv> prover (Sat 1 (Neg (Con (Pro A) (Neg (Pro A)))))\<close>
